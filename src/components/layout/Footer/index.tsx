1613c626df1218345cbeb56116c929ecc911726a
Douglas Domingos <doug.domingossilva@gmail.com>
2023-11-02 22:46:34 -0300

    chore: add color to footer link

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -2,7 +2,10 @@ const Footer = () => (
   <footer className='flex flex-col gap-2 py-4 text-sm justify-center text-center w-full'>
     <span>Douglas Domingos &copy; {new Date().getFullYear()}</span>
     <span>
-      Made with <a href='https://nextjs.org'>Next.js 14</a>
+      Made with{' '}
+      <a className='text-primary underline' href='https://nextjs.org'>
+        Next.js 14
+      </a>
     </span>
   </footer>
 );
