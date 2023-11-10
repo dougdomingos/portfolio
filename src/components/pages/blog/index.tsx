5e130f120174a8537cf3a0d3333c316880633dc9
Douglas Domingos <doug.domingossilva@gmail.com>
2023-11-10 12:43:23 -0300

    feat: add min-height to Blog section

--- src/components/pages/blog/index.tsx
+++ src/components/pages/blog/index.tsx
@@ -2,7 +2,7 @@ import Searchbar from '@components/Searchbar';
 import PostList from './PostList';
 
 const Blog = () => (
-  <section className='flex flex-col items-center py-10 gap-6'>
+  <section className='flex flex-col items-center py-10 gap-6 min-h-[72.5vh]'>
     <Searchbar />
     <PostList />
   </section>
