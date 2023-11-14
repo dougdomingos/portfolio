9f810674c1fda480cd75650373358df9f24bb710
Douglas Domingos <doug.domingossilva@gmail.com>
2023-11-14 12:58:05 -0300

    feat: add custom title to pages

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,11 @@
 import Container from '@components/layout/Container';
 import Blog from '@components/pages/blog';
 import { getPostsMetadata } from '@lib/getPostsMetadata';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Douglas Domingos | Blog',
+};
 
 export default function BlogPage() {
   const posts = getPostsMetadata();
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,10 @@
 import Container from '@components/layout/Container';
 import Projects from '@components/pages/projects';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Douglas Domingos | Projects',
+};
 
 export default function ProjectPage() {
   return (
