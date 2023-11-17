0fd186700a5762e929513dd5ee930b63bac4a826
Douglas Domingos <doug.domingossilva@gmail.com>
2023-11-17 00:44:47 -0300

    feat: add dark-mode styles to Searchbar

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,16 +1,22 @@
 import { MagnifyingGlass } from '@phosphor-icons/react/dist/ssr';
+import clsx from 'clsx';
 
 interface SearchbarProps {
   handleSearch: (value: string) => void;
 }
 
 const Searchbar = ({ handleSearch }: SearchbarProps) => (
-  <div className='relative overflow-hidden rounded-md bg-white-light transition hover:ring-primary hover:ring-2 flex w-full max-w-lg items-center shadow-md'>
+  <div
+    className={clsx(
+      'overflow-hidden rounded-md bg-white-light flex w-full max-w-lg items-center shadow-md',
+      'hover:ring-primary hover:ring-2 transition',
+      'dark:bg-black-light dark:shadow-none'
+    )}>
     <MagnifyingGlass size={20} className='ml-4' />
     <input
       type='search'
       placeholder='Search for title or #topic...'
-      className='py-3 px-4 outline-none w-full'
+      className='py-3 px-4 outline-none bg-transparent w-full'
       onChange={(event) => handleSearch(event.target.value)}
     />
   </div>
