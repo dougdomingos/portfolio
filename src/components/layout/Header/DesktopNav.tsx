f83bad33ffe6c1b41f7c574b2b4c5d43baaae033
Douglas Domingos <doug.domingossilva@gmail.com>
2023-11-29 13:18:15 -0300

    feat(DesktopNav): add new hover style on link siblings

--- src/components/layout/Header/DesktopNav.tsx
+++ src/components/layout/Header/DesktopNav.tsx
@@ -9,14 +9,16 @@ const DesktopNav = ({ links, currentPage }: Navigation) => {
   return (
     <>
       <nav>
-        <ul className='flex gap-2'>
+        <ul className='flex gap-2 group'>
           {links.map((link) => (
             <li
               key={link.label}
               className={clsx(
                 currentPage.startsWith(link.url) &&
                   'font-bold text-primary-dark bg-primary-light dark:text-primary-light dark:bg-primary-dark',
-                'rounded-md transition-colors duration-300 hover:bg-primary-light dark:hover:bg-primary-dark',
+                'rounded-md transition-[color,opacity,background] duration-300',
+                'hover:bg-primary-light dark:hover:bg-primary-dark',
+                'group-hover:opacity-60 hover:!opacity-100',
               )}>
               <Link
                 href={link.url}
