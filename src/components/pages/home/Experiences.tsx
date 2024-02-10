0b1366b847129400fce6b1366f87a926b266f2bb
Douglas Domingos <doug.domingossilva@gmail.com>
2024-02-10 20:50:03 -0300

    refactor(Home): use spread operator to pass JobCard props

--- src/components/pages/home/Experiences.tsx
+++ src/components/pages/home/Experiences.tsx
@@ -7,13 +7,7 @@ const Experiences = () => (
     <ol className='ml-2 md:ml-8 flex flex-col gap-8 border-l border-gray-300 dark:border-gray-600'>
       {jobs.map((job) => (
         <li key={job.title}>
-          <JobCard
-            time={job.time}
-            title={job.title}
-            company={job.company}
-            description={job.description}
-            current={job.current}
-          />
+          <JobCard {...job} />
         </li>
       ))}
     </ol>
