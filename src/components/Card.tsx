c4fb21c737a5a21ac148c377e0c9f64d5fcad5b6
Douglas Domingos <doug.domingossilva@gmail.com>
2024-02-11 08:07:58 -0300

    refactor(projects): move Link tag to Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,30 +1,33 @@
+'use client';
+
+import Link from 'next/link';
+
+import { Repository } from '@customTypes/Repository';
 import clsx from 'clsx';
 
 import TopicTag from './TopicTag';
 
-type CardProps = {
-  name: string;
-  description: string;
-  topics: string[];
+const Card = ({ name, description, topics, html_url }: Repository) => {
+  return (
+    <Link href={html_url} target='_blank'>
+      <article
+        className={clsx(
+          'overflow-hidden flex flex-col shadow-md rounded-md w-[300px] bg-white-light',
+          'hover:shadow-lg hover:-translate-y-1 transition-[transform,box-shadow] duration-300 hover:ring-2 hover:ring-primary',
+          'dark:shadow-none dark:bg-black-light',
+        )}>
+        <div className='flex flex-col gap-2 p-4'>
+          <h2 className='text-lg font-display font-bold'>{name}</h2>
+          <p className='h-full'>{description}</p>
+          <ul className='flex gap-2 flex-wrap'>
+            {topics.map((tag) => (
+              <TopicTag key={tag} tag={tag} />
+            ))}
+          </ul>
+        </div>
+      </article>
+    </Link>
+  );
 };
-
-const Card = ({ name, description, topics }: CardProps) => (
-  <article
-    className={clsx(
-      'overflow-hidden flex flex-col shadow-md rounded-md w-[300px] bg-white-light',
-      'hover:shadow-lg hover:-translate-y-1 transition-[transform,box-shadow] duration-300 hover:ring-2 hover:ring-primary',
-      'dark:shadow-none dark:bg-black-light',
-    )}>
-    <div className='flex flex-col gap-2 p-4'>
-      <h2 className='text-lg font-display font-bold'>{name}</h2>
-      <p>{description}</p>
-      <ul className='flex gap-2 flex-wrap'>
-        {topics.map((tag) => (
-          <TopicTag key={tag} tag={tag} />
-        ))}
-      </ul>
-    </div>
-  </article>
-);
 
 export default Card;
--- src/components/pages/projects/ProjectShowcase.tsx
+++ src/components/pages/projects/ProjectShowcase.tsx
@@ -1,5 +1,3 @@
-import Link from 'next/link';
-
 import { use } from 'react';
 
 import Card from '@components/Card';
@@ -13,13 +11,7 @@ const ProjectShowcase = () => {
       <ul className='flex flex-wrap justify-center gap-4'>
         {repositories.map((repo) => (
           <li key={repo.name}>
-            <Link href={repo.html_url} target='_blank'>
-              <Card
-                name={repo.name}
-                description={repo.description}
-                topics={repo.topics}
-              />
-            </Link>
+            <Card {...repo} />
           </li>
         ))}
       </ul>
