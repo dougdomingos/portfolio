64a96d59a57bfe3d84688f742cef5d1c88dd6b35
Douglas Domingos <doug.domingossilva@gmail.com>
2024-02-11 07:51:20 -0300

    refactor(blog): replace Link tag with useRouter to navigate to posts

--- src/components/pages/blog/PostPreview.tsx
+++ src/components/pages/blog/PostPreview.tsx
@@ -1,26 +1,39 @@
+import { useRouter } from 'next/navigation';
+
+import { formatDate } from '@lib/formatDate';
 import clsx from 'clsx';
 import { Post } from 'contentlayer/generated';
 
 import TopicTag from '../../TopicTag';
-import { formatDate } from '@lib/formatDate';
 
-const PostPreview = (post: Post) => (
-  <article
-    className={clsx(
-      'flex flex-col max-w-sm gap-2 bg-white-light p-4',
-      'dark:bg-black-light',
-    )}>
-    <span className='text-sm text-black dark:text-white-dark italic'>
-      {formatDate(new Date(post.postDate), 'short')}
-    </span>
-    <h2 className='text-lg font-bold font-display'>{post.title}</h2>
-    <p>{post.description}</p>
-    <ul className='flex gap-2'>
-      {post.topics.map((topic) => (
-        <TopicTag key={topic} tag={`#${topic}`} />
-      ))}
-    </ul>
-  </article>
-);
+const PostPreview = (post: Post) => {
+  const router = useRouter();
+
+  const handlePostClick = () => {
+    router.push(post.url);
+  };
+
+  return (
+    <article
+      onClick={handlePostClick}
+      className={clsx(
+        'flex flex-col max-w-sm gap-2 bg-white-light p-4 shadow-md rounded-md overflow-hidden',
+        'hover:-translate-y-1 hover:shadow-lg hover:ring-2 hover:ring-primary',
+        'transition-[transform,box-shadow] duration-300',
+        'dark:bg-black-light dark:shadow-none',
+      )}>
+      <span className='text-sm text-black dark:text-white-dark italic'>
+        {formatDate(new Date(post.postDate), 'short')}
+      </span>
+      <h2 className='text-lg font-bold font-display'>{post.title}</h2>
+      <p className='h-full'>{post.description}</p>
+      <ul className='flex gap-2'>
+        {post.topics.map((topic) => (
+          <TopicTag key={topic} tag={`#${topic}`} />
+        ))}
+      </ul>
+    </article>
+  );
+};
 
 export default PostPreview;
--- src/components/pages/blog/index.tsx
+++ src/components/pages/blog/index.tsx
@@ -1,13 +1,10 @@
 'use client';
 
-import Link from 'next/link';
-
 import { useState } from 'react';
 
 import PostPreview from '@components/pages/blog/PostPreview';
 import Searchbar from '@components/pages/blog/Searchbar';
 import { filterPosts } from '@lib/filterPosts';
-import clsx from 'clsx';
 import { Post } from 'contentlayer/generated';
 
 interface BlogProps {
@@ -26,16 +23,7 @@ const Blog = ({ posts }: BlogProps) => {
       <Searchbar handleSearch={handleSearch} />
       <div className='flex flex-wrap justify-center w-full gap-6'>
         {filterPosts(posts, search).map((post) => (
-          <Link
-            key={post.title}
-            href={post.url}
-            className={clsx(
-              'shadow-md rounded-md overflow-hidden',
-              'dark:shadow-none hover:-translate-y-1 hover:shadow-lg hover:ring-2 hover:ring-primary',
-              'transition-[transform,box-shadow] duration-300',
-            )}>
-            <PostPreview {...post} />
-          </Link>
+          <PostPreview key={post.title} {...post}/>
         ))}
       </div>
     </section>
