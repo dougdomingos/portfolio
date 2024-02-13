02471bb02163ebd77559b89c2506627e56fa1f26
Douglas Domingos <doug.domingossilva@gmail.com>
2024-02-13 10:27:10 -0300

    refactor(Experiences): change JobCard styling

--- src/components/pages/home/Experiences.tsx
+++ src/components/pages/home/Experiences.tsx
@@ -4,7 +4,7 @@ import jobs from './jobs';
 const Experiences = () => (
   <section className='flex flex-col gap-6'>
     <h2 className='text-2xl font-display font-bold'>Experiences</h2>
-    <ol className='ml-2 md:ml-8 flex flex-col gap-8 border-l border-gray-300 dark:border-gray-600'>
+    <ol className='py-2 ml-2 md:ml-8 flex flex-col gap-8 border-l border-gray-300 dark:border-gray-600'>
       {jobs.map((job) => (
         <li key={job.title}>
           <JobCard {...job} />
--- src/components/pages/home/JobCard.tsx
+++ src/components/pages/home/JobCard.tsx
@@ -2,27 +2,25 @@ import { Job } from '@customTypes/Job';
 import { formatDate } from '@lib/formatDate';
 import clsx from 'clsx';
 
-interface JobCardProps extends Job { }
+interface JobCardProps extends Job {}
 
-const JobCard = ({
-  time,
-  title,
-  company,
-  description,
-}: JobCardProps) => {
-  const timeSpan =
-    `${formatDate(time.start, 'long')} - ${time.end ? formatDate(time.end, 'long') : 'Current'}`;
+const JobCard = ({ time, title, company, description }: JobCardProps) => {
+  const timeSpan = `${formatDate(time.start, 'long')} - ${
+    time.end ? formatDate(time.end, 'long') : 'Current'
+  }`;
 
   return (
     <div className='relative ml-4 md:ml-6'>
       <div
         className={clsx(
           time.end ? 'bg-primary-dark' : 'bg-primary scale-125',
-          'absolute top-1.5 -left-6 md:-left-8 rounded-full w-4 h-4',
+          'absolute top-1 -left-6 md:-left-8 rounded-full w-4 h-4',
         )}
       />
-      <span className='italic'>{timeSpan}</span>
-      <h3 className='font-bold text-xl font-display'>
+      <span className='inline-block text-sm italic opacity-80 mb-1'>
+        {timeSpan}
+      </span>
+      <h3 className='font-bold text-xl font-display mb-2'>
         {title} &bull; {company}
       </h3>
       <p className='text-lg'>{description}</p>
