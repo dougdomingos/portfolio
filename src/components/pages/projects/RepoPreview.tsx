d6944fb77d071018583351b4dd9c1c5b1bb55d60
Douglas Domingos <doug.domingossilva@gmail.com>
2024-02-13 10:29:10 -0300

    refactor(RepoPreview): change RepoPreview styling

--- src/components/pages/projects/RepoPreview.tsx
+++ src/components/pages/projects/RepoPreview.tsx
@@ -7,16 +7,14 @@ import { Repository } from '@customTypes/Repository';
 const RepoPreview = ({ name, description, topics, html_url }: Repository) => {
   return (
     <Link href={html_url} target='_blank'>
-      <Card className='w-[300px]'>
-        <div className='flex flex-col gap-2'>
-          <h2 className='text-lg font-display font-bold'>{name}</h2>
-          <p className='h-full'>{description}</p>
-          <ul className='flex gap-2 flex-wrap'>
-            {topics.map((tag) => (
-              <TopicTag key={tag} tag={tag} />
-            ))}
-          </ul>
-        </div>
+      <Card className='max-w-sm md:max-w-xs'>
+        <h2 className='text-lg font-display font-bold mb-[0.2em]'>{name}</h2>
+        <p className='mb-[0.6em]'>{description}</p>
+        <ul className='flex gap-2 flex-wrap'>
+          {topics.map((tag) => (
+            <TopicTag key={tag} tag={tag} />
+          ))}
+        </ul>
       </Card>
     </Link>
   );
